087f6f44419ddb340f7a982a3d33ad762a62474f
SALMA NAFISA QURROTU'AINI <145917431+SalmaNafisa@users.noreply.github.com>
2023-11-29 21:33:57 +0700

    Add files via upload

---
+++ UmurBarangAntik56/src/umurbarangantik56/BarangAntik.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package umurbarangantik56;
+
+/**
+ *
+ * @author ASUS TUF
+ */
+public class BarangAntik {
+     int umur;
+    
+    public BarangAntik(int umur){
+      this.umur = umur;  
+    }
+    
+    public void tampilUmur(){
+        System.out.println("Umur Barang Antik ini adalah : " +umur+ " tahun ");
+        
+    }
+    
+}
+
---
+++ UmurBarangAntik56/src/umurbarangantik56/UmurBarangAntik56.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package umurbarangantik56;
+
+/**
+ *
+ * @author ASUS TUF
+ * Nama : Salma Nafisa Qurrotu'Aini
+ * Nim : 22205038
+ * Kelas : Teknik Informatika
+ * Matakuliah : PBO1
+ * Semester : 3
+ */
+
+public class UmurBarangAntik56 {
+      public static void main(String[] args) {
+        Radio rdo = new Radio(234);
+        rdo.setName("Radio AM");
+        System.out.println("Nama Barang Antik : " + rdo.getName());
+        rdo.tampilUmur();
+    }
+
+}
